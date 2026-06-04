ab4fcb10685aac36ea96376a3a9cb4bb394809ef
Josie <godoyjosie68@gmail.com>
2026-06-04 23:23:25 +0800

    .

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,15 @@ const config = getDefaultConfig(__dirname);
 // Disable server-side rendering features
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'];
 
+// Configure public path for GitHub Pages subdirectory
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      // Add custom headers if needed
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = config;
