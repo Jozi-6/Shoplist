544da07a1c8a45c6ffd297abda4fefad8be6f787
Josie <godoyjosie68@gmail.com>
2026-06-23 08:19:31 +0800

    updates

--- fix-paths.js
+++ fix-paths.js
@@ -9,8 +9,14 @@ let html = fs.readFileSync(indexPath, 'utf8');
 // Replace asset paths to include /Shoplist/ prefix
 html = html.replace(/href="\/manifest\.json"/g, 'href="/Shoplist/manifest.json"');
 html = html.replace(/href="\/favicon\.ico"/g, 'href="/Shoplist/favicon.ico"');
+html = html.replace(/href="\/favicon\.png"/g, 'href="/Shoplist/favicon.png"');
 html = html.replace(/src="\/_expo\/static\/js\/web\//g, 'src="/Shoplist/_expo/static/js/web/');
-html = html.replace(/href="\/sw\.js"/g, 'href="/Shoplist/sw.js"');
+html = html.replace(/href="\/_expo\/static\/css\/web\//g, 'href="/Shoplist/_expo/static/css/web/');
+html = html.replace(/src="\/_expo\/static\/js\/web\//g, 'src="/Shoplist/_expo/static/js/web/');
+
+// Remove service worker reference if it exists (we removed it from the app)
+html = html.replace(/<script[^>]*sw\.js[^>]*><\/script>/g, '');
+html = html.replace(/href="\/sw\.js"/g, '');
 
 // Write the modified index.html
 fs.writeFileSync(indexPath, html);
